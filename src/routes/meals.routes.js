8cd576af9eeb39f4330e5b6c05e7330ea692698d
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2022-05-22 22:06:57 +0200

    Fix auth route in meals

--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authController = require("../controllers/auth.controller");
+const authController = require("../controllers/authentication.controller");
 const router = express.Router();
 const mealController = require("../controllers/meal.controller");
 
