19160e0cbbe626144452a3a488154074e17b5c03
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2022-05-22 22:39:20 +0200

    Revert meals router

--- src/routes/meals.routes.js
+++ src/routes/meals.routes.js
@@ -3,11 +3,12 @@ const authController = require("../controllers/authentication.controller");
 const router = express.Router();
 const mealController = require("../controllers/meal.controller");
 
-router.get("/api/meal", mealController.getAllMeals)
-    router.get("/api/meal/:id", mealController.getMeal)
-    router.delete("/api/meal/:id", authController.validateToken, mealController.deleteMeal)
-    router.post("/api/meal", authController.validateToken, mealController.validateMeal, mealController.addMeal)
-    router.put("/api/meal/:id", authController.validateToken, mealController.validateMeal, mealController.updateMeal)
+router
+    .get("/api/meal", mealController.getAllMeals)
+    .get("/api/meal/:id", mealController.getMeal)
+    .delete("/api/meal/:id", authController.validateToken, mealController.deleteMeal)
+    .post("/api/meal", authController.validateToken, mealController.validateMeal, mealController.addMeal)
+    .put("/api/meal/:id", authController.validateToken, mealController.validateMeal, mealController.updateMeal)
 
 
 
