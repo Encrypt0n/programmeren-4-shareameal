9d5e3473e83cc6da6593777c53bc8fe625151101
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2022-07-03 22:59:53 +0200

    Fix controllers v5

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,10 +8,12 @@ const userController = require('../controllers/user.controller');
 const authController = require('../controllers/authentication.controller')
 
 router.post("/user", userController.validateUser, userController.addUser)
+
+router.get("/user/profile", authController.validateToken, userController.getUserProfile)
   
   router.get("/user/:userId", authController.validateToken, userController.getUserById)
 
-  router.get("/user/profile", authController.validateToken, userController.getUserProfile)
+  
   
   /*router.get('/user/profile/:userId', (req, res) => {
     res.status(400).json({
