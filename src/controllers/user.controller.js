e4ec7f6552a075ccd4b03072d45bbeb1b9918a60
Bas van Turnhout <ba.vanturnhout@student.avans.nl>
2022-07-03 20:59:40 +0200

    Fix get User v35

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -121,7 +121,7 @@ let controller = {
       });
   },
   getUserById: (req, res, next) => {
-    const userId = req.params.id;
+    const userId = req.params.userId;
     pool.query(
         `SELECT * FROM user WHERE id =${userId}`,
         (err, results, fields) => {
